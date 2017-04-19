cdb372b65d348d4186f494408bd5f93b3a1e0ae6
Christian Touzel <ctouzel@gmail.com>
2017-04-19 12:12:20 -0400

    Ajout d'un fichier

---
+++ constants_spotdriver.php
@@ -0,0 +1,119 @@
+<?php
+
+define("VERSION", "0.1.8.00010");
+define("TITLE", "SpotDriver Web");
+
+define("SPOTIFY_KEY","78bed64849d944b995b022f4d49e6b69");
+define("SPOTIFY_AUTH","7972affa3dd840a79b299663f1ed57e8");
+define("LOGIN_RETURN","http://www.touzel.com/dev/result.php");
+define("USER_CHRIS","ctouzel");
+define("USER_SIMON","simontouzel2004");
+define("USER_PHIL","atomicprout");
+define("USER_DISCOVER","spotifydiscover");
+define("USER_SPOTIFY","spotify");
+define("USER_CANADA","spotify_canada");
+
+define("FILE_LOG", "cache/log.txt");
+define("FILE_TOKEN", "cache/token.txt");
+
+define("LOG_VERBOSE", true);
+define("LOG_MAXLINES", 50);
+
+define("LIBRARY_COOL","6fC2JPPxTIsg6c6wxXperG"); 
+define("LIBRARY_FOLK","54wf0R2bOpZLUEc8AX5A5K"); 
+define("LIBRARY_OLDFIELD","7brWUvEDHuCPsxAkGcjeRI"); 
+define("LIBRARY_BASEBALL","5sgT3r0Z6EaUaDucWsCD36"); 
+define("LIBRARY_COUNTRYSOUL","6iiWfRbQkJSRlBHQImxmqe");
+define("LIBRARY_ACADIE","1iFCga3sle3Hz24VSIAoWS");
+define("LIBRARY_COFFEESHOP","7CNmDabawWGEfLyHv19ZVN");
+define("LIBRARY_FRANCOFOLK","43tFeElFXwP4PhYOsg3YwV");
+define("LIBRARY_DESERTROCK","0uMouORtHqKhWsKLTIFJow");
+define("LIBRARY_PARTY","5zq3q8l2uz00KIy95zaqxD");
+define("LIBRARY_1968","7eLtv7X4IB8T6A8uRFfK3u");
+define("LIBRARY_DIRTYROCK","1QqQcZUzhwE7SkQpmJPAkC");
+define("LIBRARY_CHRISTMAS","2p4nvHnIBKhnjyynIJ1NZn");
+define("LIBRARY_HEARTLAND_CHRISTMAS","7LRmOwpKRK8X9eLkjwZFl2");
+define("LIBRARY_REELS","17mXkQxZEQ7jRKFWdVjXDl");
+define("LIBRARY_LATIN","76FbG0r03Rqobp6jF2Odom");
+define("LIBRARY_LAB","5dY3hzkhZxbJJwY8m4ecGL");
+define("LIBRARY_BUZZ","0aFsBjW8rEYNzV3mBU2KK2");
+define("LIBRARY_SMASH","1FKeMFdR9w11iwdNfysjgd");
+define("LIBRARY_OUTLAW","3gqmucra2jDNOFao8JNI4Y");
+define("LIBRARY_PORTLAND","3SQZBtqHy3SewmppLIKbLp");
+define("LIBRARY_FUN","5EZ472UoZpaywXLLf2V13X");
+define("LIBRARY_BUENA","0mRX7s2quDEwl5p1CsgPhV");
+
+define("LIBRARY_DISCOVERWEEKLY","37i9dQZEVXcVzU1GmELhHe"); // spotify:user:spotify:playlist:37i9dQZEVXcVzU1GmELhHe
+define("LIBRARY_RELEASERADAR","37i9dQZEVXbjeNIUDcG0ps"); // spotify:user:spotify:playlist:37i9dQZEVXbjeNIUDcG0ps
+
+define("PLAYLIST_SMASH","1nQcuE632t0tX8O8CfQMu0"); 
+define("PLAYLIST_COOL","5M0fufmEyJlcuRchpPqxXP"); 
+define("PLAYLIST_BUZZ","1V4Tr7fJOAzli1VmjMP5tT");
+define("PLAYLIST_LAB","5GxyD3lHjPYImjIXdeUlBT");
+define("PLAYLIST_LATIN","5RymIqe7mGgWYJSFEroxv2");
+define("PLAYLIST_OLDFIELD","2zNngRciRgsvlp6ZZjtRt7"); 
+define("PLAYLIST_FOLK","7ozk1aoay2EQyVhr88ZRFq"); 
+define("PLAYLIST_BASEBALL","0q5Kbb29pEE7yrr2E8WPKV"); 
+define("PLAYLIST_CHRISTMAS","51w3mIHKRG2dE7fhql1Y8H");
+define("PLAYLIST_COUNTRYSOUL","2PRnSsrdo2ebvbsOE7ONkM");
+define("PLAYLIST_DIRTYROCK","6ZVSeS96Mo5jcViX1cQdo0");
+define("PLAYLIST_1968","7wKgvtB8N1McwXvCFzdfdG");
+define("PLAYLIST_PARTY","3nscWpqBe5Av7tS3vmFqJd");
+define("PLAYLIST_HEARTLAND","1cpumVT9wN1c9Vo7Ekyvqp");
+define("PLAYLIST_HEARTLAND2","3LUt3z2D8SVTrBp4ObOt5j");
+define("PLAYLIST_FRANCOFOLK","0ocQJeEWomORvawQHqvDXR");
+define("PLAYLIST_COFFEESHOP","4gsuHisCNBBbUdsfxHJp5Q");
+define("PLAYLIST_DESERTROCK","1TJEei5zOa0BIKzSeTLsJp");
+define("PLAYLIST_ACADIE","19TcJ99hzMR2AJZYt8ATgn");
+define("PLAYLIST_NICOLE","31OUfY13lsZT3yOyA0DCum");
+define("PLAYLIST_PHIL","5AvL7HH1jqwia8ewOx7hux");
+define("PLAYLIST_SIMON","1o37CSshCiuaPYoo58wfe9");
+define("PLAYLIST_CHRISTIAN","630UPES1qjaivR4cH46Vhg");
+define("PLAYLIST_CHRISTIAN_PLUS","5hDE64Xe5KEMSmtsxCHnHF");
+define("PLAYLIST_FAMILY","2jCOv7KYvy4rXbh2XUfs8G");
+define("PLAYLIST_COUPLE","6LRfNlgCSxvXPPQY6xS2Qk");
+define("PLAYLIST_OUTLAW","0o5qSvHO6mzpxmgcMTqGng");
+define("PLAYLIST_PORTLAND","4PhpkVkJietCEBSCsKF7Z0");
+define("PLAYLIST_FUN","0MIIOsstccampe85JXpSUB");
+define("PLAYLIST_BUENA","53ziloTn5R1rkSgZj7jJS9");
+
+define("CLASSICS_NICOLE","0GGlI2aXwnA5O5QNGfjJae");
+define("CLASSICS_SIMON","4YZxcUVeBoW34QibnQPrG7");
+define("CLASSICS_PHIL","6iV5aBxjmpYuMDNgAT0HQv");
+define("CLASSICS_HEARTLAND","0q1R8yX3PRn5dnObItyopP");
+define("CLASSICS_GREAT_HEARTLAND","4wUvFDeDdDW9tzZ9vQQzEP"); 
+define("CLASSICS_CHRISTIAN","2c9s3vL1BWCtu4zLcwAdqk");
+define("CLASSICS_GREAT_CHRISTIAN","2vSWo7EDpftdU34OttzOvP"); 
+
+define("TOP_SONGS_NICOLE","5mWM3KYdMOo6F0NYy3xxrs");
+define("TOP_SONGS_CHRISTIAN","3jgVVfRKMnT4ZyiW7Ja0hs");
+define("TOP_SONGS_SIMON","5FhvfwsxgGry4vhWryg3i1");
+define("TOP_SONGS_PHIL","6e6XxUh99chkvJiCDSAtLw"); 
+define("TOP_SONGS_HEARTLAND","5u3vupqo2EAogOPV8XQ5BK");
+define("TOP_ALBUMS_HEARTLAND","6BIBoHbBO21FoCniEeT294");
+define("TOP_ALBUMS_CHRISTIAN","2wuo93awZhIA43kN3NaEgS");
+define("TOP_ALBUMS_NICOLE","7yrgdYUVYasMoBQwX02Seu");
+
+
+/**
+	CONSTANTS FROM C# DESKTOP APP
+
+        // MAIN PLAYLISTS - FAMILY
+        public static string PLAYLIST_KIDS = "4LOqlNOgCb60D0Fhr4E0HY";
+        public static string PLAYLIST_COUPLE_PLUS_SIMON = "3IQdHaZnOL51Ippu5yLMRn";
+        public static string PLAYLIST_COUPLE_PLUS_PHIL = "5CifYYn9aL1AyPYG354Vnt";
+
+        // PLAYLISTS SOURCES
+        public static Playlist SPOTIFY_HOT_HITS_CANADA = new Playlist() { USER = "spotify_canada", ID = "4oBxQ0AN36SDuRoBXwtsXf", NAME = "Hot Hits Canada" };
+        public static Playlist SPOTIFY_HITS_REWIND = new Playlist() { USER = "spotify", ID = "4MATto5gVBtUNCq2ASrFf4", NAME = "Hits Rewind" };
+        public static Playlist SPOTIFY_TRENDING = new Playlist() { USER = "topsify", ID = "1KNl4AYfgZtOVm9KHkhPTF", NAME = "Trending" };
+        public static Playlist SPOTIFY_FRESH_FINDS = new Playlist() { USER = "spotify", ID = "3rgsDhGHZxZ9sB9DQWQfuf", NAME = "Fresh Finds" };
+        public static Playlist SPOTIFY_SEREINEMENT_ACCOUSTIQUE = new Playlist() { USER = "spotify_canada", ID = "5P6OKRC0nvjyB42tMaI8kn", NAME = "Sereinement Accoustique" };
+        public static Playlist SPOTIFY_PULSE_AMERICANA = new Playlist() { USER = "spotify", ID = "3xNptuWRxxBb4bb3PEP6rS", NAME = "Pulse of Americana" };
+        public static Playlist SPOTIFY_RED_DIRT = new Playlist() { USER = "spotify", ID = "7r3Xe4UBkkEtolAJEkvik8", NAME = "Red Dirt" };
+        public static Playlist SPOTIFY_ELECTRO_NOW = new Playlist() { USER = "spotify", ID = "1GQLlzxBxKTb6tJsD4RxHI", NAME = "electroNOW" };
+        public static Playlist SPOTIFY_BONNE_HUMEUR = new Playlist() { USER = "spotify_canada", ID = "3GgeU4bxrwZJ5cEu8IGGMk", NAME = "Bonne Humeur" };
+        public static Playlist SPOTIFY_SOUTHERN_GOTHIC = new Playlist() { USER = "spotify", ID = "6jOKJ9uMXxnGM9Bh3rujY3", NAME = "Southern Gothic" };
+*/
+
+?>
